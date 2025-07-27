c1dfdcd3db5d37f1e1cca9c2b5116b5eb40d7108
bryambepz <bryamparra2709@gmail.com>
2025-07-26 20:24:06 -0500

    creacion del dashboard para la vista y sus pantallas

--- TEST/Front/prueba_netby/src/app/app.routes.ts
+++ TEST/Front/prueba_netby/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component').then(t => t.DashboardComponent),
+        children: [
+            {
+                path: 'home',
+                loadComponent: () => import('./dashboard/pages/home/home.component').then(t => t.HomeComponent),
+            },
+            {
+                path: 'producto',
+                loadComponent: () => import('./dashboard/pages/producto/producto.component').then(t => t.ProductoComponent),
+            },
+            {
+                path: 'transaccion',
+                loadComponent: () => import('./dashboard/pages/transaccion/transaccion.component').then(t => t.TransaccionComponent),
+            },
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+    }
+];
---
+++ TEST/Front/prueba_netby/src/app/dashboard/pages/transaccion/transaccion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaccion',
+  standalone: true,
+  imports: [],
+  templateUrl: './transaccion.component.html',
+  styleUrl: './transaccion.component.scss'
+})
+export class TransaccionComponent {
+
+}
